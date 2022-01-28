80662576959885d5abf82f9da505922913e9a9d0
Maurizio <proiettomaurizio97@gmail.com>
2022-01-28 16:45:10 +0100

    print database data on page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,4 +3,11 @@
     <h1>
         List Comics
     </h1>
+    <ul>
+        @foreach ($comics as $comic)
+            <li>
+                {{ $comic -> title}} (pag.{{ $comic -> pages }})
+            </li>
+        @endforeach
+    </ul>
 @endsection
