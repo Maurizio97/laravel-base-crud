054684d079aeac0b55e6425a789a2ff8047325c7
Maurizio <proiettomaurizio97@gmail.com>
2022-01-28 17:19:59 +0100

    frontend complete

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,15 +1,18 @@
 @extends('layouts.main-layout')
 @section('content')
+<section>
     <h1>
         List Comics
     </h1>
     <ul>
         @foreach ($comics as $comic)
             <li>
+                {{ $comic -> id }}-
                 <a href=" {{route('show', $comic -> id)}}">
                     {{ $comic -> title}} (pag.{{ $comic -> pages }})
                 </a>
             </li>
         @endforeach
     </ul>
+</section>
 @endsection
